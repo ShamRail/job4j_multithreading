0e230f0c652cfe261120f59d8ddad13fc7a1cf96
ShamRail <rail1999g102r@mail.ru>
2020-07-04 01:58:13 +0500

    3.1. Прерывание блокированной нити. [#267413]

--- chapter_001/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,25 +3,28 @@ package ru.job4j.concurrent;
 public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
-        try {
-            //char[] symbols = {'\\', '|', '/'};
-            String[] symbols = {".", "..", "..."};
-            int index = 0;
-            while (!Thread.currentThread().isInterrupted()) {
+        String[] symbols = {".", "..", "..."};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
                 System.out.print("\rLoading " + symbols[index]);
                 index = (index + 1) % symbols.length;
                 Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                System.out.println();
+                System.out.println(Thread.currentThread().isInterrupted());
+                Thread.currentThread().interrupt();
+                System.out.print("\rLoaded");
+                System.out.println();
+                System.out.println(Thread.currentThread().isInterrupted());
             }
-        } catch (InterruptedException e) {
-            //e.printStackTrace();
-            System.out.print("\rLoaded");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(10000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        Thread.sleep(3000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
         progress.interrupt(); //
     }
 
