0d0617f4c3a1ce7c51b403cd9ae5009ccb79eef0
ShamRail <rail1999g102r@mail.ru>
2020-07-05 19:25:22 +0500

    4. Скачивание файла с ограничением. [#320430] - UDP

--- chapter_001/src/main/java/ru/job4j/concurrent/FileDownload.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -3,6 +3,7 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FileDownload {
@@ -16,6 +17,10 @@ public class FileDownload {
             throw new IllegalArgumentException();
         }
         // "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml"
+        fileDownload1(args);
+    }
+
+    private static void fileDownload1(String[] args) throws MalformedURLException, InterruptedException {
         String file = args[0];
         int kbs = Integer.parseInt(args[1]);
         URL url = new URL(file);
@@ -37,4 +42,31 @@ public class FileDownload {
         long end = System.currentTimeMillis();
         System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
     }
+
+    private static void fileDownload2(String[] args) throws MalformedURLException, InterruptedException {
+        String file = args[0];
+        int kbs = Integer.parseInt(args[1]);
+        URL url = new URL(file);
+        long start = System.currentTimeMillis();
+        try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[kbs * KB];
+            int bytesRead;
+            do {
+                long startLoading = System.currentTimeMillis();
+                bytesRead = in.read(dataBuffer, 0, kbs * KB);
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long endLoading = System.currentTimeMillis();
+                long duration = endLoading - startLoading;
+                if (duration < SECOND) {
+                    Thread.sleep(SECOND - duration);
+                }
+            } while (bytesRead != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
+    }
+
 }
