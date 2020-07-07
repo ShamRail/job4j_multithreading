9d676af729f90f54269fe5a72ffc562e4c62d22d
ShamRail <rail1999g102r@mail.ru>
2020-07-07 11:34:36 +0500

    4. Скачивание файла с ограничением. [#320430] - UDP

--- chapter_001/src/main/java/ru/job4j/concurrent/FileDownload.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -2,8 +2,6 @@ package ru.job4j.concurrent;
 
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FileDownload {
@@ -16,57 +14,82 @@ public class FileDownload {
         if (args.length != 2) {
             throw new IllegalArgumentException();
         }
-        // "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml"
-        fileDownload1(args);
+        new Thread(new FileDownload1(args)).start();
     }
 
-    private static void fileDownload1(String[] args) throws MalformedURLException, InterruptedException {
-        String file = args[0];
-        int kbs = Integer.parseInt(args[1]);
-        URL url = new URL(file);
-        long start = System.currentTimeMillis();
-        try (BufferedInputStream in = new BufferedInputStream(url.openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte[] dataBuffer = new byte[kbs * KB];
-            int bytesRead;
-            do {
-                bytesRead = in.read(dataBuffer, 0, kbs * KB);
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                if (bytesRead >= kbs * KB) {
-                    Thread.sleep(SECOND);
-                }
-            } while (bytesRead != -1);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static class FileDownload1 implements Runnable {
+
+        private final String[] args;
+
+        public FileDownload1(String[] args) {
+            this.args = args;
         }
-        long end = System.currentTimeMillis();
-        System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
+
+        @Override
+        public void run() {
+            try {
+                String file = args[0];
+                int kbs = Integer.parseInt(args[1]);
+                URL url = new URL(file);
+                long start = System.currentTimeMillis();
+                try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+                     FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+                    byte[] dataBuffer = new byte[kbs * KB];
+                    int bytesRead;
+                    do {
+                        bytesRead = in.read(dataBuffer, 0, kbs * KB);
+                        fileOutputStream.write(dataBuffer, 0, bytesRead);
+                        if (bytesRead >= kbs * KB) {
+                            Thread.sleep(SECOND);
+                        }
+                    } while (bytesRead != -1);
+                }
+                long end = System.currentTimeMillis();
+                System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
+            } catch (Exception e){
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
     }
 
-    private static void fileDownload2(String[] args) throws MalformedURLException, InterruptedException {
-        String file = args[0];
-        int kbs = Integer.parseInt(args[1]);
-        URL url = new URL(file);
-        long start = System.currentTimeMillis();
-        try (BufferedInputStream in = new BufferedInputStream(url.openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte[] dataBuffer = new byte[kbs * KB];
-            int bytesRead;
-            do {
-                long startLoading = System.currentTimeMillis();
-                bytesRead = in.read(dataBuffer, 0, kbs * KB);
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long endLoading = System.currentTimeMillis();
-                long duration = endLoading - startLoading;
-                if (duration < SECOND) {
-                    Thread.sleep(SECOND - duration);
-                }
-            } while (bytesRead != -1);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static class FileDownload2 implements Runnable {
+
+        private final String[] args;
+
+        public FileDownload2(String[] args) {
+            this.args = args;
         }
-        long end = System.currentTimeMillis();
-        System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
+
+        @Override
+        public void run() {
+            try {
+                String file = args[0];
+                int kbs = Integer.parseInt(args[1]);
+                URL url = new URL(file);
+                long start = System.currentTimeMillis();
+                try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+                     FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+                    byte[] dataBuffer = new byte[kbs * KB];
+                    int bytesRead;
+                    do {
+                        long startLoading = System.currentTimeMillis();
+                        bytesRead = in.read(dataBuffer, 0, kbs * KB);
+                        fileOutputStream.write(dataBuffer, 0, bytesRead);
+                        long endLoading = System.currentTimeMillis();
+                        long duration = endLoading - startLoading;
+                        if (duration < SECOND) {
+                            Thread.sleep(SECOND - duration);
+                        }
+                    } while (bytesRead != -1);
+                }
+                long end = System.currentTimeMillis();
+                System.out.printf("Time: %.2f", ((double) (end - start)) / 1000);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
     }
 
 }
