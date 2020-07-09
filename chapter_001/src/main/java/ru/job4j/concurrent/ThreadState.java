af8d99c7d59de0b8f3973dcbd2a01434b54c8a44
ShamRail <rail1999g102r@mail.ru>
2020-07-09 17:59:17 +0500

    1.1. Состояние нити. [#320431] - UDP

--- chapter_001/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,7 +12,7 @@ public class ThreadState {
         System.out.println("second: " + first.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println("first: " + first.getState());
             System.out.println("second: " + first.getState());
         }
